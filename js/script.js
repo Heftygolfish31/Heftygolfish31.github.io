5d136f777e7aea79976c1f6a6ab4ae6b7b039eb5
Heftygolfish31 <cubiccommand@proton.me>
2023-06-06 17:13:27 +0100

    Movover

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-// Write JavaScript here 
-var welsh_flag = false;
+// var welsh_flag = false;
 
-var language_change_link = document.getElementById('change-language');
-language_change_link.onclick = change_language;
+// var language_change_link = document.getElementById('change-language');
+// language_change_link.onclick = change_language;
 
-function change_language() {
+// function change_language() {
   
-    if (welsh_flag == true) {
-      welsh_flag = false;
-    } else {
-      welsh_flag = true;
-    }
+//     if (welsh_flag == true) {
+//       welsh_flag = false;
+//     } else {
+//       welsh_flag = true;
+//     }
   
-    alert("languaged changed. welsh is now " + welsh_flag);
-    return false;
-}
+//     alert("languaged changed. welsh is now " + welsh_flag);
+//     return false;
+// }
