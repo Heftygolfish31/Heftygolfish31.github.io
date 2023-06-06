09f04ad4d8e0e222b2dbfcf7d63a6b77bc58bd18
cubic <cubiccommand@proton.me>
2023-06-06 23:01:37 +0100

    Mobile Check 3

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ function isMobileBrowser() {
     const mobileRegExp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Tablet/i;
     return mobileRegExp.test(navigator.userAgent);
   }
-  
+
+
+if (isMobileBrowser()) {
+// Code to run if the website is being viewed from a mobile browser
+alert('Mobile browser detected!');
+} else {
+// Code to run if the website is not being viewed from a mobile browser
+alert('Not a mobile browser.');
+}
